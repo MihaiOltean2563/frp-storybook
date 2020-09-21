e4d9a985a72c5af1a865de777a0ff3640610da19
Morhys Malak <dev@morhys.com>
2020-09-21 09:34:17 +0100

    extract css and dynamically load wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frp';
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  themes = ['beko', 'hotpoint'];
+
+  ngOnInit() {
+    this.loadStyle('beko.css');
+  }
+
+  loadStyle(styleName: string) {
+    const head = this.document.getElementsByTagName('head')[0];
+
+    let themeLink = this.document.getElementById('theme') as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = styleName;
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'theme';
+      style.rel = 'stylesheet';
+      style.href = `${styleName}`;
+
+      head.appendChild(style);
+    }
+  }
 }
