b3d8fab327f0d6eb2d2e21b857f7334d8bf16c09
Morhys <morhys.m@googlemail.com>
2020-09-29 09:27:07 +0100

    latest

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,19 +1,27 @@
-import { addParameters } from '@storybook/angular'; // or any other type of storybook
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from '../documentation.json';
-setCompodocJson(docJson);
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-// import { themes } from '@storybook/theming';
+import { addDecorator, addParameters } from '@storybook/angular'; // or any other type of storybook
+import { withRootAttribute } from 'storybook-addon-root-attribute';
+
+// global
+addDecorator(withRootAttribute);
 
 /* Override defaults for dark and light themes */
 addParameters({
-  darkMode: {
-    // dark: { ...themes.dark, appBg: 'goldenrod' },
-    // light: { ...themes.normal, appBg: 'red' },
-    current: 'dark'
-  },
-  viewport: {
-    viewports: INITIAL_VIEWPORTS, //comment out to use basic options e.g Small Mobile  / Large Mobile / Tablet
+  rootAttribute: {
+    tooltip: true,
+    root: 'body',
+    defaultState: {
+      name: 'Default',
+      value: null
+    },
+    states: [
+      {
+        name: 'Beko',
+        value: 'beko'
+      },
+      {
+        name: 'Hotpoint',
+        value: 'hotpoint'
+      }
+    ]
   }
 });
-
